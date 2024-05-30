ffba153ea8192214b14d8269772bb045b18bb400
unknown <maiaagustina.barrionuevo@estudiantes.unahur.edu.ar>
2024-05-30 07:14:00 -0300

    fix

--- src/controllers/carreras.controller.js
+++ src/controllers/carreras.controller.js
@@ -1,5 +1,5 @@
 const {Carreras} = require('../db/models')
-
+const {Materia} = require('../db/models')
 
 const controller = {}
 
@@ -18,9 +18,33 @@ controller.carreraById = carreraById
 const crearCarrera = async (req, res) => {
     const carrera = await Carreras.create(req.body)
     res.status(201).json(carrera)
-    return carrera
     
 }
 controller.crearCarrera = crearCarrera
+
+const crearMateriaEnCarreraById = async (req, res) => {
+    const id = req.params.id
+    const materia = await Materia.create({ ...req.body, carreraId: id})
+    const carrera  = await Carreras.findByPk(id)
+    const carreraActualizada = await Carreras.findByPk(id, {
+        include: { model: Materia, as: 'materias' } 
+    });
+    
+    res.status(201).json(carreraActualizada)
+    
+}
+controller.crearMateriaEnCarreraById = crearMateriaEnCarreraById
+
+const getMateriasDeCarreraById = async (req, res) => {
+    const id = req.params.id
+    const carrera  = await Carreras.findByPk(id, {
+        include: {model: Materia, as: 'materias'}
+    })
+    const materias = carrera.materias
+
+    res.status(200).json(materias)
+    
+}
+controller.getMateriasDeCarreraById = getMateriasDeCarreraById
 
 module.exports = controller
--- src/routes/carreras.route.js
+++ src/routes/carreras.route.js
@@ -5,4 +5,7 @@ const route = Router()
 route.get('/carreras', carrerasController.getAllCarreras )
 route.get('/carreras/:id', carrerasController.carreraById )
 route.post('/carreras', carrerasController.crearCarrera)
+route.post('/carreras/:id/materia', carrerasController.crearMateriaEnCarreraById)
+route.get('/carreras/:id/materias', carrerasController.getMateriasDeCarreraById)
+
 module.exports = route
