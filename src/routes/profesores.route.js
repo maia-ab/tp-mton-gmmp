bf0f916fa07424addb57746a78629400005db7c3
unknown <maiaagustina.barrionuevo@estudiantes.unahur.edu.ar>
2024-06-02 19:39:36 -0300

    terminamos error 500 y arreglamos getCursosEnProfesoresById

--- src/routes/profesores.route.js
+++ src/routes/profesores.route.js
@@ -1,5 +1,5 @@
 const {Router} = require('express')
-const {Profesores} = require('../db/models')
+const {Profesores, Cursos} = require('../db/models')
 const profesoresController = require('../controllers/profesores.controller')
 const middlewareProfesores = require('../middlewares/middlewares')
 const profesoresSchema = require('../schemas/profesores.schemas')
@@ -10,7 +10,7 @@ route.get('/profesores', profesoresController.getAllProfesores)
 route.get('/profesores/:id', middlewareProfesores.existsById(Profesores), profesoresController.profesorById)
 route.post('/profesores', middlewareProfesores.validaSchema(profesoresSchema), profesoresController.crearProfesor)
 route.put('/profesores/:id', middlewareProfesores.existsById(Profesores), profesoresController.actualizarProfesor)
-route.delete('/profesores/:id', middlewareProfesores.existsById(Profesores), /*Agregar error 500 */ profesoresController.eliminarProfesor)
+route.delete('/profesores/:id', middlewareProfesores.existsById(Profesores), middlewareProfesores.existeRelacionConRegistro(Profesores, Cursos), profesoresController.eliminarProfesor)
 route.get('/profesores/:id/cursos', middlewareProfesores.existsById(Profesores), profesoresController.getCursosEnProfesoresById)
 
 module.exports = route
